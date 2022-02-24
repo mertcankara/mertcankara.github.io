e18a0fd510b89dcf8ebe8a2069dc4e18a037676e
mertcankara <00mertcankara00@gmail.com>
2022-02-24 18:02:00 +0300

    first commit

---
+++ _next/static/pE7rdYwvd3hcwNtofTeVQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6798bd1f84b05420.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
