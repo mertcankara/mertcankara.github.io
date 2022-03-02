01cd27f342175290cd3012c94b32d62a20977855
mertcankara <mertcan.kara@agem.com.tr>
2022-03-02 23:32:57 +0300

    update

--- _next/static/o4PPLJqy9Km3WUauVtKmD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6798bd1f84b05420.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
