268d72cc7f92b90a7fb7ba6fdfe7caa164786c78
mertcankara <mertcan.kara@agem.com.tr>
2022-04-11 00:07:28 +0300

    update

---
+++ _next/static/chunks/pages/index-fe660f6f77654ae3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8581:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(8552)}])}},function(n){n.O(0,[774,888,179],(function(){return _=8581,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
